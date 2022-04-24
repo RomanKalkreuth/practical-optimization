11e4974d4ca69d09b952b2f8700db5eee07ea3df
Roman Kalkreuth <64356752+RomanKalkreuth@users.noreply.github.com>
2022-04-24 19:46:53 +0200

    Test functions added for optim.py

--- optim.py
+++ optim.py
@@ -5,37 +5,50 @@ Example for the use of minimize in scipy.optimize for BFGS
 import numpy as np
 from scipy.optimize import minimize
 
-def function(X):
-    """Test function for the optimization"""
+def quadratic_sum(X):
+    """ Quadratic sum problem """
     s = 0
     for i, x in enumerate(X):
-        s += x**i
+        s += x ** 2
     return s
 
-def gradient(X):
-    """Gradient calculation for the test function"""
-    grad = np.zeros(len(X))
-    for i,x in enumerate(X):
-        grad[i] = i*x
-    return grad
 
-dim = 10
+def quadratic_sum_scaled(X):
+    """ Scaled quadratic sum problem """
+    s = 0
+    for i, x in enumerate(X):
+        s += (i + 1) ** 2 * x ** 2
+    return s
+
+
+def schwefel(X):
+    """ Schwefel's problem """
+    s1 = 0
+    s2 = 0
+    n = len(X)
+    for i, x in enumerate(X):
+        s2 = 0
+        for j in range(i, n):
+            s2 += x ** 2
+    s1 += s2 ** 2
+    return s1
+
+dim = 5
 
 # define range for input
-min, max = 0.0, 10.0
+min, max = -10.0, 10.0
 
 # calculate the starting point uniformly distributed by chance
-x0 = np.random.randint(min,max,dim)
-
-# Run the optimization
-res = minimize(function, x0, method='BFGS', jac=gradient)
+xz = np.random.randint(min, max, dim)
+res = minimize(schwefel, xz, method='BFGS')
 
 # Get the solution and the function value
 sol = res['x']
-fs = function(sol)
+fs = quadratic_sum(sol)
 
 # Print the total evaluations and the solution
 print('Total Evaluations: %d' % res['nfev'])
 print('Solution: f(%s) = %.5f' % (sol, fs))
+
 
 
